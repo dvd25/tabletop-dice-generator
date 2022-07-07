48b0b424120e4b29d6a411dbd659249442889cf9
dvd25 <davidhoang2506@gmail.com>
2022-07-07 17:33:56 +1200

    Add UI changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function rollDice(){
     let maxNum = Number(document.form1.numDiceFace.value);
-    let randomNumber = Math.floor(Math.random() * maxNum);
+    let randomNumber = Math.floor(Math.random() * maxNum) + 1;
     document.form1.total.value = randomNumber;
 }
